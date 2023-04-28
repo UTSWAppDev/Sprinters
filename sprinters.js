cf2723be75fbdfcf7c8dfcecc20d3ab440e99340
hongzhaoji <hongzhao.ji@utsouthwestern.edu>
2023-04-28 18:11:56 -0500

    Blob Storage Test

--- sprinters.js
+++ sprinters.js
@@ -1,32 +1,19 @@
 const express = require('express');
 const multer = require('multer');
-const mysql = require('mysql');
-
-// Create a MySQL connection pool
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'sprinters',
-  password: 'Ddjsddjs1!',
-  database: 'Sprinters',
-});
+const { BlobServiceClient } = require('@azure/storage-blob');
 
 // Create an Express app
 const app = express();
 
 // Set up Multer to handle file uploads
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/');
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname);
-  }
-});
-
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
+// Create a Blob Service Client
+const blobServiceClient = BlobServiceClient.fromConnectionString('DefaultEndpointsProtocol=https;AccountName=sprinterappstorage;AccountKey=cMGeIF09/yxf+a0QWcK84tE2dgcwthq62O7oobeUNizu/7NqSZxgQS1w7rmm7vZSWQylzdA/qq6R+AStWjZmeQ==;EndpointSuffix=core.windows.net');
+
 // Define a route to handle file uploads
-app.post('/upload', upload.single('file'), (req, res, next) => {
+app.post('/upload', upload.single('file'), async (req, res, next) => {
   const file = req.file;
 
   if (!file) {
@@ -35,17 +22,15 @@ app.post('/upload', upload.single('file'), (req, res, next) => {
     return next(error);
   }
 
-  // Insert file details into the database
-  const sql = 'INSERT INTO VIDEOS (name, location) VALUES (?, ?)';
-  const values = [file.originalname, file.path];
+  // Upload the file to Blob Storage
+  const containerName = 'videos';
+  const blobName = Date.now() + '-' + file.originalname;
+  const containerClient = blobServiceClient.getContainerClient(containerName);
+  const blockBlobClient = containerClient.getBlockBlobClient(blobName);
+  const uploadResult = await blockBlobClient.upload(file.buffer, file.buffer.length);
 
-  pool.query(sql, values, (err, result) => {
-    if (err) {
-      return next(err);
-    }
-
-    res.send('File uploaded successfully');
-  });
+  // Send response
+  res.send('File uploaded successfully');
 });
 
 // Start the server
