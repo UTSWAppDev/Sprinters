c83d84ba2ff03db274fed2fa5b82d32f8f371139
hongzhaoji <hongzhao.ji@utsouthwestern.edu>
2023-05-04 06:50:17 -0500

    get code

--- sprinters.js
+++ sprinters.js
@@ -52,6 +52,45 @@ app.post('/upload', upload.single('file'), (req, res, next) => {
   });
 });
 
+
+// define a route for GET request at /listpics
+app.get('/listpics', (req, res) => {
+  // execute the SQL query
+  pool.query('SELECT * FROM VIDEOS', (err, rows) => {
+    if (err) throw err;
+
+    // create an HTML table with the video names and links
+    const tableRows = rows.map(row => {
+      return `<tr><td>${row.originalname}</td><td><a href="${row.path}">Watch video</a></td></tr>`;
+    }).join('');
+
+    // return an HTML page with the table
+    const html = `
+      <html>
+        <head>
+          <title>List of Videos</title>
+        </head>
+        <body>
+          <h1>List of Videos</h1>
+          <table>
+            <thead>
+              <tr>
+                <th>Video Name</th>
+                <th>Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              ${tableRows}
+            </tbody>
+          </table>
+        </body>
+      </html>
+    `;
+
+    res.send(html);
+  });
+});
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
